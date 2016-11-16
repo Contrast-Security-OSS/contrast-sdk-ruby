8d41b67808cb4baa86f2a0adf3d8bca8bd111f7b
Galen Palmer <galen.palmer@contrastsecurity.com>
2016-11-16 13:06:42 -0500

    wip: initial smoke tests for remainins server classes

---
+++ spec/contrast/user_api/alerts_api_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Contrast::UserApi::AlertsApi do
+  it 'can be instantiated' do
+    expect(Contrast::UserApi::AlertsApi.new).to_not be_nil
+  end
+end
---
+++ spec/contrast/user_api/libraries_api_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Contrast::UserApi::LibrariesApi do 
+  it 'can be instantiated' do
+    expect(Contrast::UserApi::LibrariesApi.new).to_not be_nil
+  end
+end
+
